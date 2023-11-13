4a7120ee9e82442e2027fd549670a28472f885f5
MasonD-007 <masondrake305@gmail.com>
2023-11-12 22:33:16 -0800

    Added style

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,32 @@
-import { useState } from 'react'
+import { useContext } from 'react'
 import { Userpage } from './pages/Userpage'
 import { Login } from './pages/Login'
-import { BrowserRouter as Router } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom'
+import { AuthContext } from './Context/AuthContext'
+import './style.scss';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const { currentUser } = useContext(AuthContext)
+
+  const ProtectedRoute = ({ children }) => {
+    if (!currentUser) {
+      return <Navigate to="/login" />
+    }
+
+    return children
+  }
 
   return (
     <>
       <Router>
-        <Userpage />
+        <Routes path='/'>
+          <Route index element={
+          <ProtectedRoute>
+            <Userpage />
+            </ProtectedRoute>
+          } />
+          <Route path='/login' element={<Login />} />
+        </Routes>
       </Router>
     </>
   )
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,9 +1,41 @@
 import React from 'react'
+import { useNavigate, Link } from 'react-router-dom';
+import { signInWithEmailAndPassword } from 'firebase/auth';
+import { auth } from '../firebase';
 
 export const Login = () => {
+const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const email = e.target[0].value;
+    const password = e.target[1].value;
+
+    try {
+      await signInWithEmailAndPassword(auth, email, password);
+      console.log('Login successful');
+      navigate('/');
+    } catch(error) {
+
+      console.log(error);
+      return alert("Invalid email or password")
+    };
+
+  }
 
 
   return (
-    <div>Login</div>
+    <div className='formContainer'>
+        <div className='loginPage'>
+            <span className='logo'>Financial Literacy</span>
+            <span className='title'>Login</span>
+            <form onSubmit={handleSubmit}>
+                <input type='email' placeholder='Email' />
+                <input type='password' placeholder='Password' />
+                <button>Login</button>
+            </form>
+            <p>Don't have an account? <Link to='/register'>Register</Link></p>
+        </div>
+    </div>
   )
 }
