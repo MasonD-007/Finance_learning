b312cb82f5a326117d3413013c144f6ea4e877a7
MasonD-007 <masondrake305@gmail.com>
2023-11-25 22:12:16 -0800

    sign out of homepage

--- src/components/Guidecolumn.jsx
+++ src/components/Guidecolumn.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import {SearchBar} from './Searchbar';
+import { signOut } from "firebase/auth";
+import { auth } from '../firebase';
 
 export const GuideColumn = () => {
   return (
